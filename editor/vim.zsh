b8f584d38fa720efa303ccc4cd2578baa63de265
coxley <codey@cwv.net>
2015-05-18 17:12:51 -0400

    added vundle check

---
+++ editor/vim.zsh
@@ -0,0 +1,9 @@
+#  Vim
+
+# Install Vundle if not already installed
+if [ ! -d "~/.vim/bundle/Vundle.vim" ]
+then
+    git clone \
+        https://github.com/gmarik/Vundle.vim.git \
+        ~/.vim/bundle/Vundle.vim
+fi
