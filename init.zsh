4aeb93045b2a6aeb44825cfab4f8a8d40a2df175
Codey Oxley <coxley@users.noreply.github.com>
2015-06-03 11:56:05 -0700

    add virtualenvwrapper installation check. if not, install to user dir

--- init.zsh
+++ init.zsh
@@ -12,3 +12,8 @@ source $SOURCE/functions/functions.zsh
 # env
 source $SOURCE/env/env.zsh
 source $SOURCE/env/shell.zsh
+
+# install virtualenvwrapper if not installed
+# make sure to install it to user to avoid affecting the system
+pip_freeze=$(pip freeze 2> /dev/null)
+[[ $pip_freeze != *"virtualenvwrapper"* ]] && pip install --user virtualenvwrapper
