fa7f869ca4959c68e99722f865e4108b89981722
Codey Oxley <coxley@users.noreply.github.com>
2015-06-03 11:59:27 -0700

    Adding ~/.local/bin to PATH

--- env/env.zsh
+++ env/env.zsh
@@ -25,6 +25,7 @@ export PATH="$PATH:$HOME/bin"
 export PATH="$PATH:$HOME/.gem/ruby/2.1.0/bin"
 export PATH="$PATH:/opt/vc/bin"
 export PATH="$PATH:$HOME/.rvm/bin"
+export PATH="$PATH:$HOME/.local/bin"
 
 # history
 HIST_STAMPS="yyyy-mm-dd"
