c647514be2b639a6a171caaa3e18c47c393e1671
Codey Oxley <codey@yelp.com>
2015-09-07 20:13:24 -0700

    changing arcanist path

--- env/env.zsh
+++ env/env.zsh
@@ -28,7 +28,7 @@ export PATH="$PATH:$HOME/.local/bin"
 if which ruby >/dev/null && which gem >/dev/null; then
     PATH="$(ruby -rubygems -e 'puts Gem.user_dir')/bin:$PATH"
 fi
-arcanist_path='/usr/local/src/arcanist/bin'
+arcanist_path='~/.local/share/arcanist/bin'
 [ -x "$arcanist_path" ] &&
     export PATH="$PATH:$arcanist_path"
 
