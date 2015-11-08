6b3933a84f16aade4d4f91dc3fea8ee41f84a9ef
Codey Oxley <codey@yelp.com>
2015-11-08 00:42:37 -0800

    Added filename grouping to ls

--- aliases/aliases.zsh
+++ aliases/aliases.zsh
@@ -40,7 +40,7 @@ alias -g NUL=' > /dev/null 2>&1'
 
 # Files
 #
-LS_CMD='ls --group-directories-first --color=tty'
+LS_CMD='ls --group-directories-first --color=tty -X'
 $LS_CMD 1>&2 2> /dev/null
 if [ "$?" != 0 ]; then
     alias ls=$LS_CMD
